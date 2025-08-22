6cb418761ada810680332574f4b419179fc3333c
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-08-22 13:45:43 +0530

    fetch data TMDB data fetch and built main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className='w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
+    <div className='absolute top-0 left-0 right-0 w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
       <img className='w-44'
         src={LOGO}
         alt='logo'/>
